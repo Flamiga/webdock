812c94ba27f8aa5c1f239ac48de98382b6c208da
Flamiga <eaafla@students.eaaa.dk>
2018-09-07 15:02:23 +0200

    webdock

---
+++ detsmukkeistoej/js/main.js
@@ -0,0 +1,16 @@
+
+
+
+
+var video = document.getElementById("myvideo");
+    var btn = document.getElementById("myBtn");
+
+    function myFunction() {
+      if (video.paused) {
+        video.play();
+        btn.innerHTML = "Pause";
+      } else {
+        video.pause();
+        btn.innerHTML = "Play";
+      }
+    }
